b31d88487f652f46a631ea2f4b3c2e6dedcb47c8
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-04-27 20:24:47 +0545

    Day08-Env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const connectToDatabase = require("./database");
 const app = express();
@@ -16,6 +17,6 @@ app.get("/about", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
--- database/index.js
+++ database/index.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 async function connectToDatabase() {
-  await mongoose.connect(
-    "mongodb+srv://iamshareefg_db_user:Rowhit2002@cluster0.acaqmp2.mongodb.net/?appName=Cluster0",
-  );
-  console.log("Database Connected")
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Database Connected");
 }
 
-module.exports = connectToDatabase
+module.exports = connectToDatabase;
