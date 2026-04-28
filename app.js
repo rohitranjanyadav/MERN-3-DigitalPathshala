13e98609da42714fcc643da4958b3ee14832088a
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-04-28 17:42:47 +0545

    create blogModel

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDatabase = require("./database");
+
 const app = express();
+app.use(express.json())
 
 connectToDatabase();
 
@@ -10,6 +12,13 @@ app.get("/", (req, res) => {
     message: "Home page",
   });
 });
+
+app.post("/blog",(req,res)=>{
+  console.log(req.body)
+  res.status(200).json({
+    message:"Blog API hit"
+  })
+})
 
 app.get("/about", (req, res) => {
   res.json({
